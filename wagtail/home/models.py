e4e8bd54a07550bd0693b57636dde7692c1c16a1
Noelle Anderson <noelle@noelleubuntucinnamon.localdomain>
2020-06-15 12:47:27 -0400

    Start index & base.html customization

--- wagtail/home/models.py
+++ wagtail/home/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    intro = models.CharField(
+        max_length=140,
+        blank=True,
+    )
+
+    content_panels = Page.content_panels + [
+       FieldPanel('intro'),
+   ] 
